97d4f4bc49d96f5337926fec7046a91d86de4c6b
Frédéric Delorme <frederic.delorme@gmail.com>
2013-03-06 16:47:23 +0100

    #874237 Refactor Unit test intialization layer.

--- src/test/java/fr/mcgivrer/applications/angulargames/test/dao/GenericDaoTest.java
+++ src/test/java/fr/mcgivrer/applications/angulargames/test/dao/GenericDaoTest.java
@@ -3,6 +3,7 @@
  */
 package fr.mcgivrer.applications.angulargames.test.dao;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -51,30 +52,31 @@ public class GenericDaoTest extends DatabaseConfiguration {
 
 		EntityTransaction tx = em.getTransaction();
 		try {
-			tx.begin();
-			em.createNativeQuery("truncate table game").executeUpdate();
-			tx.commit();
-			tx.begin();
-			Game game1 = new Game("Dragon's Dogma", "X360",
-					"http://ecx.images-amazon.com/images/I/81bfGYT1I8L._SL80_.jpg");
-			em.persist(game1);
-			Game game2 = new Game("L.A. Noire", "X360",
-					"http://ecx.images-amazon.com/images/I/81NZFcx-%2BBL._SL80_.jpg");
-			em.persist(game2);
-			Game game3 = new Game("Sonic Generation", "X360",
-					"http://ecx.images-amazon.com/images/I/81hlSLQEeoL._SL80_.jpg");
-			em.persist(game3);
-			Game game4 = new Game("Alan Wake", "X360",
-					"http://ecx.images-amazon.com/images/I/41PpoZ62wrL._SL80_.jpg");
-			em.persist(game4);
+			BigInteger count = (BigInteger) em
+					.createNativeQuery("select count(*) from game")
+					.getResultList().get(0);
+			if (count.intValue() == 0) {
+				tx.begin();
+				Game game1 = new Game("Dragon's Dogma", "X360",
+						"http://ecx.images-amazon.com/images/I/81bfGYT1I8L._SL80_.jpg");
+				em.persist(game1);
+				Game game2 = new Game("L.A. Noire", "X360",
+						"http://ecx.images-amazon.com/images/I/81NZFcx-%2BBL._SL80_.jpg");
+				em.persist(game2);
+				Game game3 = new Game("Sonic Generation", "X360",
+						"http://ecx.images-amazon.com/images/I/81hlSLQEeoL._SL80_.jpg");
+				em.persist(game3);
+				Game game4 = new Game("Alan Wake", "X360",
+						"http://ecx.images-amazon.com/images/I/41PpoZ62wrL._SL80_.jpg");
+				em.persist(game4);
 
-			// Back just created games list
-			dataGame.put(game1.getName(), game1);
-			dataGame.put(game2.getName(), game2);
-			dataGame.put(game3.getName(), game3);
-			dataGame.put(game4.getName(), game4);
-
-			tx.commit();
+				// Back just created games list
+				dataGame.put(game1.getName(), game1);
+				dataGame.put(game2.getName(), game2);
+				dataGame.put(game3.getName(), game3);
+				dataGame.put(game4.getName(), game4);
+				tx.commit();
+			}
 		} catch (Exception e) {
 			tx.rollback();
 		}
